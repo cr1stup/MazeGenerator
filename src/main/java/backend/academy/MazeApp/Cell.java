4315a7713b413a5e0768c1216181978f990b09da
cr1stup <scaleupgo@gmail.com>
2024-10-16 15:13:54 +0300

    feat(Cell): add method getCost
    add cost of cells and realize getCost method

--- src/main/java/backend/academy/MazeApp/Cell.java
+++ src/main/java/backend/academy/MazeApp/Cell.java
@@ -10,9 +10,27 @@ public enum Cell {
     END('B');
 
     private final char view;
+    private static final int WALL_COST = Integer.MAX_VALUE;
+    private static final int PATH_COST = 1;
+    private static final int COIN_COST = -5;
+    private static final int TRAP_COST = 5;
 
     Cell(char view) {
         this.view = view;
+    }
+
+    public int getCost() {
+        int cost;
+
+        switch (this) {
+            case WALL -> cost = WALL_COST;
+            case PATH -> cost = PATH_COST;
+            case COIN -> cost = COIN_COST;
+            case TRAP -> cost = TRAP_COST;
+            default -> cost = 0;
+        }
+
+        return cost;
     }
 
     @Override
