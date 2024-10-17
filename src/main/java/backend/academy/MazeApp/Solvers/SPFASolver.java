88c24ba34a2ef76fa0c11ac4143aad498f69d171
cr1stup <scaleupgo@gmail.com>
2024-10-17 07:48:05 +0300

    feat(Solvers): add Shortest-Path-Faster-Algorithm solver

---
+++ src/main/java/backend/academy/MazeApp/Solvers/SPFASolver.java
@@ -0,0 +1,40 @@
+package backend.academy.MazeApp.Solvers;
+
+import backend.academy.MazeApp.Cell;
+import backend.academy.MazeApp.Coordinate;
+import backend.academy.MazeApp.Maze;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+public class SPFASolver extends MazeSolver {
+
+    @Override
+    public List<Coordinate> solve(Maze maze, Coordinate start, Coordinate end) {
+        costs = new int[maze.height()][maze.width()];
+        cameFrom = doSPFASolve(maze.grid(), start);
+
+        return reconstructPath(cameFrom, start, end);
+    }
+
+    private Map<Coordinate, Coordinate> doSPFASolve(Cell[][] grid, Coordinate start) {
+        Queue<Coordinate> q = new LinkedList<>();
+        q.add(new Coordinate(start.row(), start.col()));
+
+        initialization(start);
+
+        while (!q.isEmpty()) {
+            Coordinate curr = q.poll();
+            int i = curr.row();
+            int j = curr.col();
+
+            checkAllDirections(i, j, grid, q);
+
+            costs = Arrays.stream(tempCosts).map(int[]::clone).toArray(int[][]::new);
+        }
+
+        return cameFrom;
+    }
+}
