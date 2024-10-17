3994b3ca8347279445a060125584d61ae62bc115
cr1stup <scaleupgo@gmail.com>
2024-10-17 11:28:12 +0300

    feat(Runner): implement class MazeRunner
    add method run that run all process

--- src/main/java/backend/academy/MazeApp/MazeRunner.java
+++ src/main/java/backend/academy/MazeApp/MazeRunner.java
@@ -1,9 +1,12 @@
 package backend.academy.MazeApp;
 
+import backend.academy.MazeApp.Generators.MazeGenerator;
+import backend.academy.MazeApp.Solvers.MazeSolver;
 import backend.academy.MazeApp.Utils.InputProcessor;
 import backend.academy.MazeApp.Utils.Renderer;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.List;
 
 public class MazeRunner {
     private final Renderer renderer;
@@ -15,6 +18,22 @@ public class MazeRunner {
     }
 
     public void run() {
+        renderer.printGreeting();
+        int height = input.inputHeight();
+        int width = input.inputWidth();
 
+        MazeGenerator generator = input.chooseGenerator();
+        Maze maze = generator.generate(height, width);
+
+        renderer.renderGridWithCoordinates(maze.grid());
+
+        Coordinate start = input.inputStart(maze.grid());
+        Coordinate end = input.inputEnd(maze.grid());
+
+        MazeSolver solver = input.chooseSolver();
+        List<Coordinate> path = solver.solve(maze, start, end);
+
+        renderer.renderGrid(maze.grid(), "Your Maze with A and B!\n");
+        renderer.renderPath(maze.grid(), path);
     }
 }
